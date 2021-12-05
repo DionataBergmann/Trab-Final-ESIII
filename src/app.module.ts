7afffdd6c7eebed9b345afb015a6812c82d20b9a
Dionata  Bergmann Podewils <dionata.podewils@outlook.com>
2021-12-05 15:57:55 -0300

    :pencil2: Fix bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppService } from './app.service';
-import { StudentsModule } from './modules/students/students.module';
 import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 import { ServicesModule } from './modules/services/services.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { ServicesModule } from './modules/services/services.module';
-import { TasksModule } from './modules/tasks/tasks.module';
-import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     sortSchema: true,
-}), StudentsModule, CollaboratorsModule, ServicesModule, TasksModule,],
+}),  CollaboratorsModule, ServicesModule, TasksModule,],
   providers: [AppService],
   
 })
